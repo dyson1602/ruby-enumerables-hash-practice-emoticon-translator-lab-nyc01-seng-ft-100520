22beef83ad157bd90b1723b160b9bc1d623da6ed
Christopher Clark <cclark000@citymail.cuny.edu>
2020-09-18 19:24:13 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,7 +16,8 @@ def get_japanese_emoticon(file_location, english_emoticon)
  
   dictionary = load_library(file_location)
   
-  dictionary.find(english_emoticon)
+  dictionary.each do |key, value|
+    key.
   
   binding.pry
  
