7facaca39cdfda2da136f5e7b2de5bd96c571e88
Christopher Clark <cclark000@citymail.cuny.edu>
2020-09-18 18:51:59 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,6 +5,9 @@ def load_library(file_location)
   dictionary = YAML.load_file(file_location)
   
   ej_dictionary = dictionary.each_with_object({}) do |(key, value), a|
+    binding.pry
+  end
+
   
   binding.pry
 
