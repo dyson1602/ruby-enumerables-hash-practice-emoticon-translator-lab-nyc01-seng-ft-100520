7208e841ec43919db42770066904cb9919f4fb1f
Christopher Clark <cclark000@citymail.cuny.edu>
2020-09-18 18:50:05 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,21 @@
-# require modules here
+require 'pry'
+require 'yaml'
 
-def load_library
-  # code goes here
+def load_library(file_location)
+  dictionary = YAML.load_file(file_location)
+  
+  binding.pry
+
+  
+  
+  
+  
 end
 
-def get_japanese_emoticon
-  # code goes here
-end
+# def get_japanese_emoticon
+#   # code goes here
+# end
 
-def get_english_meaning
-  # code goes here
-end
+# def get_english_meaning
+#   # code goes here
+# end
