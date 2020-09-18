35e91b7711b0cfbdba9154b014d0e4fffcaa66fd
Christopher Clark <cclark000@citymail.cuny.edu>
2020-09-18 19:20:46 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,7 +15,7 @@ end
 def get_japanese_emoticon(file_location, english_emoticon)
  
   dictionary = load_library(file_location)
-  jkey = dictionary.key(english_emoticon])
+  jkey = dictionary.key(english_emoticon)
   binding.pry
  
 end
